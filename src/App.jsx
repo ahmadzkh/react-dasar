2bddb445135fff252c09e56ce2ae8930b0b0d8bc
Ahmad Zaky <humami359@gmail.com>
2025-01-02 16:40:56 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Index from "./components/Index";
-import "./style.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Index from "./components/Index.jsx";
+import "./index.css";
 
 function App() {
   return (
